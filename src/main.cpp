b4c090b6cefe1c12f6be316c12fa417d32f376e1
Dirk Arnez <smalldirkalex@gmail.com>
2022-08-15 22:11:55 +0800

    - update src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main()
   cout << "Hello World!";
   char str[100];
   cout << "Enter a string: ";
-  gets(str);
+  gets(str); // cannot compile because `gets` is block in "poison.h"
   cout << "You entered: " << str;
   return 0;
 }
